f517ffddef3460b2261f98972357895330561354
Ismael Guajardo <ismael.guajardo.dev@gmail.com>
2025-01-24 16:58:01 -0700

    Create person api service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './components/app.component';
 import { SearchComponent } from './components/search/search.component';
 import { DetailsComponent } from './components/details/details.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/PersonApiService.ts
@@ -0,0 +1,27 @@
+import { environment } from '../../environments/environment';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+@Injectable({ providedIn: 'root' })
+export class PersonApiService {
+    private apiUrl = environment.API_URL + '/search';
+
+    constructor(private http: HttpClient) {}
+
+    fetchAll(params?: {
+        term?: string;
+        color?: string
+    }): Observable<any> {
+        let httpParams = new HttpParams();
+
+        if(params?.term) {
+            httpParams = httpParams.set('term', params.term.trim());
+        }
+        if(params?.color) {
+            httpParams = httpParams.set('color', params.color.trim());
+        }
+
+        return this.http.get(this.apiUrl, { params: httpParams });
+    }
+}
