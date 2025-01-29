48cfb57070a526709181fb0d7de17bb7ecac71d7
Ismael Guajardo <ismael.guajardo.dev@gmail.com>
2025-01-29 00:01:50 -0700

    Ensure value is string before trim

--- src/app/validators/term-color.validator.ts
+++ src/app/validators/term-color.validator.ts
@@ -3,8 +3,8 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 // Custom validator: Ensure either 'name' or 'color' is provided
 export function termOrColorValidator(): ValidatorFn {
   return (formGroup: AbstractControl): ValidationErrors | null => {
-    const term = formGroup.get('term')?.value?.trim();
-    const color = formGroup.get('color')?.value?.trim();
+    const term = String(formGroup.get('term')?.value || '').trim();
+    const color = String(formGroup.get('color')?.value || '').trim();
 
     // Valid if at least one field is non-empty
     return term || color ? null : { termOrColorRequired: true };
